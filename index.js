385c8d87ac8391fd2fcd2527c549ecc248b1630b
rafatalamsaifi <rafatalamsaifi@gmail.com>
2022-10-08 15:51:17 +0000

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const http = require("http");
-const fs = requier("fs");
-const hostname = "127.0.0.1";
-const port = 3000;
-const server = fs.createServer((req, res)=>{
-    res.statuscode=
-})
+
+const express = require('express');
+const app = express();
+const pug = require('pug');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(express.static('static'));
+
+app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'))
+
+
+
+// try server pug file without path.join
+
+
+app.get('/', (req, res)=>{
+    res.render('./home.pug')
+});
+
+app.get('/about', (req, res)=>{
+    res.send('This is my About Page.')
+});
+
+app.listen(port, ()=>{
+    console.log('server is running')
+});
+
