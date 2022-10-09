e18ca08c45e8d736af00fea699f1814a570af9c8
rafatalamsaifi <rafatalamsaifi@gmail.com>
2022-10-09 16:16:47 +0000

    used pug template engine

--- index.js
+++ index.js
@@ -5,25 +5,20 @@ const pug = require('pug');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('static'));
+app.use('/static', express.static('static'));
 
 app.set('view engine', 'pug');
-// app.set('views', path.join(__dirname, 'views'))
-
-
-
-// try server pug file without path.join
-
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res)=>{
-    res.render('./home.pug')
+    res.status(200).render('home')
 });
 
 app.get('/about', (req, res)=>{
-    res.send('This is my About Page.')
+    res.render('about')
 });
 
 app.listen(port, ()=>{
-    console.log('server is running')
+    console.log(`server is running ${port}`)
 });
 
